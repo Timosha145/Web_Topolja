b0104854acf2e6b63a787132503bfdd822018e64
Timosha145 <timothytopoly@gmail.com>
2023-08-31 00:03:05 +0300

    Добавьте файлы проекта.

---
+++ Web_Topolja/Data/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Topolja.Models;
+
+namespace Web_Topolja.Data.ApplicationDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<ContactData> ContactDatas { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Web_Topolja/Models/ContactData.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Topolja.Models
+{
+    public class ContactData
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
